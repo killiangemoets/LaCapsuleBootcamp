b23a3dba0cc0f7721c846e99e61771db67110ffe
Killian Gemoets <kilgem@live.fr>
2022-03-25 15:31:13 +0100

    challenge cat running

---
+++ Challenges/Day5/CatWalk/script.js
@@ -0,0 +1,29 @@
+const cat = $("#cat-run");
+const btn = $("#action");
+
+// let numImg = 2;
+// btn.click(function () {
+//   cat.attr("src", `./images/cat-${numImg}.jpg`);
+//   numImg = numImg === 13 ? 1 : numImg + 1;
+// });
+
+const speed = 50;
+let numImg = 2;
+let state = 0;
+
+var timer = null;
+btn.click(function () {
+  if (state === 0) {
+    timer = setInterval(run, speed);
+    state = 1;
+  } else {
+    clearInterval(timer);
+    state = 0;
+  }
+});
+
+function run() {
+  cat.attr("src", `./images/cat-${numImg}.jpg`);
+  numImg = numImg === 13 ? 1 : numImg + 1;
+  btn.text("Stop");
+}
