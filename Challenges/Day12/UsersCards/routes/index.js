4e3b38d220c85aece87a74b293e77afc7d6443e2
Killian Gemoets <kilgem@live.fr>
2022-04-05 11:01:22 +0200

    userscards challenge

---
+++ Challenges/Day12/UsersCards/routes/index.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var request = require("sync-request");
+var router = express.Router();
+
+const avatars = [
+  "boss",
+  "boy",
+  "girl-1",
+  "girl-2",
+  "girl-3",
+  "girl",
+  "man-2",
+  "man",
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const result = request("GET", "https://jsonplaceholder.typicode.com/users");
+  const resultObj = JSON.parse(result.body);
+
+  res.render("index", { resultObj, avatars });
+});
+
+router.get("/messages", function (req, res, next) {
+  const id = req.query.id;
+  const posts = request(
+    "GET",
+    `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+  );
+
+  const postsObj = JSON.parse(posts.body);
+
+  res.render("posts", { postsObj });
+});
+
+module.exports = router;
