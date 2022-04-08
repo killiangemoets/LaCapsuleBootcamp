8411276fef2747fe9ca4c570af1fc237fa3d8f44
Killian Gemoets <kilgem@live.fr>
2022-04-08 17:15:20 +0200

    day15

--- Challenges/Day15/gpsApp/public/javascripts/script.js
+++ Challenges/Day15/gpsApp/public/javascripts/script.js
@@ -5,11 +5,36 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.Routing.control({
-  waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
+let control = L.Routing.control({
+  waypoints: [],
+  routeWhileDragging: true,
 }).addTo(map);
 
-map.on("click", function () {
-  console.log(this);
-  const { lat, lng } = this.latlng;
+let startBtn, destBtn;
+
+// const control = L.Control;
+
+function createButton(label, container) {
+  var btn = L.DomUtil.create("button", "", container);
+  btn.setAttribute("type", "button");
+  btn.innerHTML = label;
+  return btn;
+}
+
+map.on("click", function (e) {
+  var container = L.DomUtil.create("div"),
+    startBtn = createButton("Start from this location", container),
+    destBtn = createButton("Go to this location", container);
+
+  L.popup().setContent(container).setLatLng(e.latlng).openOn(map);
+
+  L.DomEvent.on(startBtn, "click", function () {
+    control.spliceWaypoints(0, 1, e.latlng);
+    map.closePopup();
+  });
+
+  L.DomEvent.on(destBtn, "click", function () {
+    control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+    map.closePopup();
+  });
 });
