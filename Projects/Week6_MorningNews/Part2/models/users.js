8980d5b066652417b610d9b91cc43e708572dad7
Killian Gemoets <kilgem@live.fr>
2022-04-26 15:17:55 +0200

    MorningNews

---
+++ Projects/Week6_MorningNews/Part2/models/users.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "xxx Please provide a username xxx"],
+  },
+  email: {
+    type: String,
+    required: [true, "yyy Please provide an email address yyy"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "yyy Please provide a valid email yyy"],
+  },
+  password: {
+    type: String,
+    required: [true, "zzz Please provide a password zzz"],
+    minlength: [8, "zzz Your password should have at least 8 characters zzz"],
+  },
+});
+
+var userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
