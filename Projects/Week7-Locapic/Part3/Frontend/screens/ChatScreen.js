875ec1d69ba80ec15b2bd155452f2ba5012f2f42
Killian Gemoets <kilgem@live.fr>
2022-05-05 19:43:03 +0200

    reactnative

---
+++ Projects/Week7-Locapic/Part3/Frontend/screens/ChatScreen.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import { ListItem, Avatar, Button, Input, Ico } from "@rneui/themed";
+import { connect } from "react-redux";
+
+import socketIOClient from "socket.io-client";
+
+var socket = socketIOClient("http://172.20.10.4:3000");
+
+function ChatScreen(props) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [listMessages, setListMessages] = useState([]);
+
+  function addEmojis(message) {
+    // const smileEmoji = "";
+    // const sadEmoji = "";
+    // const lolEmoji = "";
+    let updateMessage = message.replaceAll(":)", "\u263A");
+    updateMessage = updateMessage.replace(/:\(/g, "\u2639");
+    updateMessage = updateMessage.replace(/:p/g, "\uD83D\uDE1B");
+    updateMessage = updateMessage.replace(/fuck[a-z]*/gi, "***");
+    return updateMessage;
+  }
+  useEffect(() => {
+    socket.on("sendMessageToAll", (newMessage) => {
+      console.log(newMessage);
+      const messageWithEmojis = addEmojis(newMessage);
+      // setListMessages((prev) => [...prev, newMessage]);
+      setListMessages([
+        ...listMessages,
+        { message: messageWithEmojis, pseudo: props.myPseudo },
+      ]);
+    });
+  }, [listMessages]);
+
+  function sendMessage() {
+    socket.emit("sendMessage", currentMessage);
+    setCurrentMessage("");
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#eee" }}>
+      <View
+        style={{
+          marginTop: 30,
+          marginBottom: 0,
+          flex: 1,
+          justifyContent: "space-between",
+          overflow: "scroll",
+        }}
+      >
+        <ScrollView
+          style={{
+            // marginTop: 30,
+            // marginBottom: 0,
+            // flex: 1,
+            // height: "80%",
+            // justifyContent: "space-between",
+            overflow: "scroll",
+          }}
+        >
+          {listMessages.map((el, i) => (
+            <ListItem key={i} bottomDivider>
+              <ListItem.Content>
+                <ListItem.Title>{el.message}</ListItem.Title>
+                <ListItem.Subtitle>{el.pseudo}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </ScrollView>
+        <KeyboardAvoidingView behavior="padding" enabled>
+          <View>
+            <Input
+              inputStyle={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                color: "#999",
+              }}
+              placeholder="Your message"
+              leftIcon={{
+                marginRight: 20,
+              }}
+              onChangeText={(value) => setCurrentMessage(value)}
+              value={currentMessage}
+            />
+            <Button
+              title="Send"
+              icon={{
+                name: "envelope",
+                type: "font-awesome",
+                size: 15,
+                color: "#fff",
+              }}
+              iconContainerStyle={{ marginRight: 18 }}
+              buttonStyle={{
+                backgroundColor: "#eb4d4b",
+                borderRadius: 3,
+              }}
+              onPress={() => sendMessage()}
+            />
+          </View>
+        </KeyboardAvoidingView>
+      </View>
+    </View>
+  );
+}
+
+function mapStateToProps(state) {
+  return { myPseudo: state.pseudo };
+}
+
+// export default ScreenMyArticles;
+export default connect(mapStateToProps, null)(ChatScreen);
+
+// export default ChatScreen;
---
+++ Projects/Week7-Locapic/Part3/Frontend/screens/HomeScreen.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import { Button, Input, Icon } from "@rneui/themed";
+import { connect } from "react-redux";
+
+function HomeScreen(props) {
+  const [pseudo, setPseudo] = useState("");
+
+  function login(username) {
+    if (username.length > 0) {
+      props.navigation.navigate("Main");
+      props.savePseudo(username);
+    }
+    // console.log(username);
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#2ecc71",
+        // backgroundImage: "url('../assets/home.jpg')",
+      }}
+    >
+      <ImageBackground
+        source={require("../assets/home.jpg")}
+        style={{
+          width: "100%",
+          height: "100%",
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Input
+          onChangeText={(value) => setPseudo(value)}
+          value={pseudo}
+          containerStyle={{
+            width: 200,
+            marginHorizontal: 50,
+            marginVertical: 10,
+            color: "rgba(78, 116, 289, 1)",
+          }}
+          inputStyle={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            color: "red",
+          }}
+          placeholder="John"
+          leftIcon={{
+            type: "font-awesome",
+            name: "user",
+            color: "#eb4d4b",
+            marginRight: 10,
+          }}
+        />
+        <Button
+          title="Login"
+          icon={{
+            name: "arrow-right",
+            type: "font-awesome",
+            size: 15,
+            color: "#eb4d4b",
+          }}
+          iconContainerStyle={{ marginRight: 10 }}
+          buttonStyle={{
+            backgroundColor: "rgba(78, 116, 289, 1)",
+            borderRadius: 3,
+          }}
+          containerStyle={{
+            //   width: 200,
+            marginHorizontal: 50,
+            marginVertical: 10,
+          }}
+          onPress={() => login(pseudo)}
+        />
+      </ImageBackground>
+    </View>
+  );
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    savePseudo: function (pseudo) {
+      dispatch({ type: "savePseudo", pseudo });
+    },
+  };
+}
+
+// function mapStateToProps(state) {
+//   return {};
+// }
+
+export default connect(null, mapDispatchToProps)(HomeScreen);
+
+// export default HomeScreen;
